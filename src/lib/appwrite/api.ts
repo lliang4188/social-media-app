392935d64ca6b3c99ac47a9eb71432b2619bd533
ryker <42561515@qq.com>
2023-11-03 21:18:29 +0800

    add creat post

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,8 +1,7 @@
 import { ID, Query } from 'appwrite'
  
-import { INewUser } from "@/types";
-import { account, appwriteConfig, avatars, database } from './config';
-import { useSignOutAccount } from '../react-query/queriesAndMutations';
+import { INewPost, INewUser } from "@/types";
+import { account, appwriteConfig, avatars, database, storage } from './config'
 
 export async function createUserAccount(user: INewUser) {
    try {
@@ -91,4 +90,109 @@ export async function signOutAccount() {
   } catch (error) {
     console.log(error)    
   }
+}
+
+export async function craetePost(post: INewPost) {
+  try {
+    // upload image to storage
+    const uploadedFile = await uploadFile(post.file[0])
+
+    if (!uploadedFile) throw Error
+
+    // Get file url
+    const fileUrl = getFilePreview(uploadedFile.$id)
+
+    if (!fileUrl) {
+      deleteFile(uploadedFile.$id)
+      throw Error
+    }
+
+    // Convert tags in an array
+    const tags = post.tags?.replace(/ /g, '').split(',') || []
+
+    // Save post to database
+
+    const newPost = await database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      ID.unique(),
+      {
+        creator: post.userId,
+        caption: post.caption,
+        imageUrl: fileUrl,
+        imageId: uploadedFile.$id,
+        location: post.location,
+        tags: tags
+      }
+    )
+    
+    if (!newPost) {
+      await deleteFile(uploadedFile.$id)
+      throw Error
+    }
+
+    return newPost
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function uploadFile(file: File) {
+  try {
+    const uploadedFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      file
+    )
+    
+    return uploadedFile
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getFilePreview(fileId: string) {
+  try {
+    const fileUrl = storage.getFilePreview(
+      appwriteConfig.storageId,
+      fileId,
+      2000,
+      2000,
+      'top',
+      100
+    )
+
+    return fileUrl
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function deleteFile(fileId: string) {
+
+  try {
+    await storage.deleteFile(
+      appwriteConfig.storageId,
+      fileId
+    )
+
+    return { status: 'ok' }
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getRecentPosts() {
+  const posts = await database.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.postCollectionId,
+    [Query.orderDesc('$createdAt'), Query.limit(20)]
+  )
+
+  if (!posts) throw Error
+  
+  return posts
+  
 }
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -4,8 +4,9 @@ import {
   useQueryClient,
   useInfiniteQuery, 
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+import { craetePost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api'
+import { INewPost, INewUser } from '@/types'
+import { QUERY_KEYS } from './queryKeys'
 
 export const useCreateUserAccount = () => {
   return useMutation({
@@ -26,4 +27,24 @@ export const useSignOutAccount = () => {
   return useMutation({
     mutationFn: signOutAccount
 })
+}
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (post: INewPost) => craetePost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+
+      })
+    }
+  })
+}
+
+export const useGetRecentPosts = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
+  })
 }
